b646c8b9922bb0b692f8fe43ee5230837a98fe06
Kristian Payne <kjp29@kent.ac.uk>
2020-09-23 21:06:01 +0100

    Updates

---
+++ precache-manifest.1d11e25b87330dee592354df9e344a3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7fda08ec5e1d8d3dbc33fce79222a1c",
+    "url": "/bin-collection/index.html"
+  },
+  {
+    "revision": "45d5de02ca66764eba99",
+    "url": "/bin-collection/static/css/main.b22fa0e6.chunk.css"
+  },
+  {
+    "revision": "522aad8d05ad93aac25c",
+    "url": "/bin-collection/static/js/2.24611759.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bin-collection/static/js/2.24611759.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d5de02ca66764eba99",
+    "url": "/bin-collection/static/js/main.a045c3a0.chunk.js"
+  },
+  {
+    "revision": "42d01b375fe380ff1981",
+    "url": "/bin-collection/static/js/runtime-main.745b8d48.js"
+  }
+]);
